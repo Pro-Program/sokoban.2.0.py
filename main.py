bddd774fb25e57f56f33fda28e8bb0ec7980f517
gordonlin2011 <64232078+gordonlin2011@users.noreply.github.com>
2020-12-16 11:38:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pygame, time
 
 pygame.init()
-width, height = 1920, 1080
+width, height = 500, 500
 backgroundColor = 100, 0, 0
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 penguin = pygame.image.load("images/penguin.png")
-penguinx = 0
-penguiny = 0
-
-while True:
+penguinx = 170
+penguiny = 170
+donenot1 = 0
+while not donenot1:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            donenot1 = 1
     screen.fill(backgroundColor)
     screen.blit(penguin,(penguinx,penguiny))
     pygame.display.flip()
