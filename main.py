65d12a0436796d65b6b74ba86f9abaf5ddd20102
gordonlin2011 <64232078+gordonlin2011@users.noreply.github.com>
2020-12-19 12:59:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ backgroundColor = 100, 0, 0
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 penguin = pygame.image.load("images/penguin.png")
-penguinx = 170
-penguiny = 170
+penguin = pygame.transform.scale(penguin, (32,32))
+penguinx = 468
+penguiny = 250
+changex = 0
+changexyz = 0
 donenot1 = 0
+
+
 while not donenot1:
     for event in pygame.event.get():
+        if pygame.KEYDOWN == event.type:
+            if event.key == pygame.K_LEFT:
+                changex -= 10
         if event.type == pygame.QUIT:
             donenot1 = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                changex = 0
+    penguinx += changex
     screen.fill(backgroundColor)
     screen.blit(penguin,(penguinx,penguiny))
     pygame.display.flip()
