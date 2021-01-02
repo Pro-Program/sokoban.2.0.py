30748e2ebcfd494c4eb25e8619cce4ef2c5a334d
gordonlin2011 <64232078+gordonlin2011@users.noreply.github.com>
2021-01-02 13:18:47 -0500

    ggg

--- main.py
+++ main.py
@@ -6,28 +6,56 @@ backgroundColor = 100, 0, 0
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-penguin = pygame.image.load("images/penguin.png")
+penguin = pygame.image.load("sokoban.2.0.py\images\penguin.png")
 penguin = pygame.transform.scale(penguin, (32,32))
 penguinx = 468
 penguiny = 250
 changex = 0
-changexyz = 0
-donenot1 = 0
+changey = 0
+done = 0
+speed = 2
 
 
-while not donenot1:
+
+
+
+while not done:
     for event in pygame.event.get():
         if pygame.KEYDOWN == event.type:
             if event.key == pygame.K_LEFT:
-                changex -= 10
+                changex -= speed
+            if event.key == pygame.K_RIGHT:
+                changex += speed
+            if event.key == pygame.K_UP:
+                changey -= speed
+            if event.key == pygame.K_DOWN:
+                changey += speed
         if event.type == pygame.QUIT:
-            donenot1 = 1
+            done = 1
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 changex = 0
+            if event.key == pygame.K_RIGHT:
+                changex = 0
+            if event.key == pygame.K_UP:
+                changey = 0
+            if event.key == pygame.K_DOWN:
+                changey = 0
     penguinx += changex
+    penguiny += changey
+    if penguinx > 500:
+        penguinx = 0
+        penguiny = 0
+    if penguinx < -1:
+        penguinx = 0
+        penguiny = 0
+    if penguiny > 500:
+        penguinx = 0
+        penguiny = 0
+    if penguiny < -1:
+        penguinx = 0
+        penguiny = 0
     screen.fill(backgroundColor)
     screen.blit(penguin,(penguinx,penguiny))
     pygame.display.flip()
     clock.tick(60)
-
